c12b22bff154c8833615468f55ad0f9ba1111448
bd-ang <110931262+bd-ang@users.noreply.github.com>
2022-08-09 13:16:57 -0700

    Create Wye_Delta_Capcitors.m

---
+++ HW1/Wye_Delta_Capcitors.m
@@ -0,0 +1,6 @@
+%% Exercise 10.2 
+% Following an analogous derivation as that in Example 10.3, replace the six resistors in Figure
+% 10.4a-b with six capacitors {C1, C2, C3, Ca, Cb, Cc}, and write a code Wye_Delta_Capacitors.m that quanti-
+% fies {Ca, Cb, Cc} in terms of {C1, C2, C3}, and vice versa, so that the two circuits are equivalent. Then, replacing
+% the five resistors in Figure 10.3d with ve capacitors {C1, C2, C3, C4, C5} and applying this result, compute the
+% equivalent capacitance C of this network.
